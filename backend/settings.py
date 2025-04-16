2fe922fc3f5caaabbcde2c949ec9d93b08239bc7
Chirag-wamanacharya12 <chiragwamanacharya42@gmail.com>
2025-04-16 17:52:40 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -110,7 +110,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbpwf32489006',
+        'NAME': 'postgres',
         'USER': 'dbpwf32489006',
         'PASSWORD': 'bv1KLSoxYls4Jo?kuu7AmPld',
         'HOST': 'serverless-eu-west-2.sysp0000.db1.skysql.com',
