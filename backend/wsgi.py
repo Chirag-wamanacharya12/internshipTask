0b340cb276bd170c42fe0a116879932647fc3aaf
Chirag-wamanacharya12 <chiragwamanacharya42@gmail.com>
2025-04-17 21:15:59 +0530

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
+# Full path to your project directory
+project_home = '/home/chirag18/usermanagement'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
+
